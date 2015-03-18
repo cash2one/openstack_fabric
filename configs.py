754eead053f895439986c3f4da1dc4bf218078c7
Zhao Zhenlong <zhaozhl@inspur.com>
2015-03-18 09:07:38 -0400

    first commit

---
+++ configs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+LOCAL_NTP_CONFIG = "./ntp.conf"
+NTP_CONFIG = "/etc/ntp.conf"
+
+AUTH_KEYS = "./authorized_keys"
+HOST_LIST = "/etc/hosts"
+
+LOCAL_CENTOS7_REPO = "./centos7.repo"
+LOCAL_EPEL7_REPO = "./epel7.repo"
+CENTOS7_REPO = "/etc/yum.repos.d/CentOS-Base.repo"
+EPEL7_REPO = "/etc/yum.repos.d/epel.repo"
+
+LOCAL_CENTOS6_REPO = "./centos6.repo"
+LOCAL_EPEL6_REPO = "./epel6.repo"
+CENTOS6_REPO = "/etc/yum.repos.d/CentOS-Base.repo"
+EPEL6_REPO = "/etc/yum.repos.d/epel.repo"
+
+LOCAL_OPENSTACK_REPO = "./openstack.repo"
+OPENSTACK_REPO = "/etc/yum.repos.d/rdo-release.repo"
+
+LOCAL_IVIRTUAL_REPO = "./ivirtual.repo"
+IVIRTUAL_REPO = "/etc/yum.repos.d/ivirtual.repo"
---
+++ openstack.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from fabric.api import *
+from configs import *
+
+env.user = "root"
+env.password = "root"
+
+controller = ['ctrl']
+
+network = ['net']
+
+compute = [
+	'com01',
+	'com02',
+	'com04',
+	'com05',
+	'com06',
+]
+
+nodes = controller + network + compute
+
+with open("/root/.ssh/id_rsa.pub", "r") as f:
+	line = f.read()
+	with open(AUTH_KEYS, "w") as g:
+		g.write(line)
+
+env.roledefs = {
+	'controller' : controller,
+	'network' : network,
+	'compute' : compute,
+	'all' : nodes,
+}
+
+@roles('all')
+def _ssh_keygen_and_gather():
+	run("ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa")
+	key = run("cat ~/.ssh/id_rsa.pub")
+	f = open(AUTH_KEYS, 'a')
+	f.write(key + "\n")
+	f.close()
+
+@roles('all')
+def _dispatch_auth_key():
+	put(AUTH_KEYS, '~/.ssh/authorized_keys')
+	run('chmod 600 ~/.ssh/authorized_keys')
+
+@roles('all')
+def _clean_auth_key():
+	run('rm -f /root/.ssh/*')
+
+@roles('all')
+def _put_hosts():
+	put(HOST_LIST, HOST_LIST)
+
+@roles('all')
+def _test_connection():
+	run('hostname')
+
+# -----------------------
+# 1. add openstack repo
+# -----------------------
+@roles('all')
+@parallel
+def _local_repo():
+	#backup origin repo
+	with cd("/etc/yum.repos.d"):
+		run('mkdir backup && mv CentOS-* backup || echo "exits"')
+	put(LOCAL_CENTOS7_REPO, CENTOS7_REPO)
+	put(LOCAL_EPEL7_REPO, EPEL7_REPO)
+	put(LOCAL_OPENSTACK_REPO, OPENSTACK_REPO)
+	run('yum clean all')
+	run('yum makecache')
+	run('yum upgrade -y')
+
+# -----------------------
+# 2. install ntp
+# -----------------------
+@roles('all')
+def _setup_ntp():
+	run("yum install -y ntp")
+	put(LOCAL_NTP_CONFIG, NTP_CONFIG)
+	run("systemctl enable ntpd.service")
+	run("systemctl restart ntpd.service")
+	run("systemctl status ntpd.service")
+
+# -----------------------
+# 3. install selinux
+# -----------------------
+@roles('all')
+def _setup_selinux():
+	run("yum install -y openstack-selinux")
+
+
+
+# ========================================== #
+#                  tasks                     #
+# ========================================== #
+
+def inject_auth():
+	execute(_dispatch_auth_key)
+
+def make_auth():
+	execute(_ssh_keygen_and_gather)
+	execute(_dispatch_auth_key)
+
+def clean_auth():
+	execute(_clean_auth_key)
+
+def update_hosts():
+	execute(_put_hosts)
+
+def setup_ntp():
+	execute(_setup_ntp)
+
+def openstack_repo():
+	execute(_local_repo)
+
+def test():
+	execute(_test_connection)
+
+def all():
+	execute(_local_repo)
+	execute(_setup_ntp)
+	execute(_setup_selinux)
+
