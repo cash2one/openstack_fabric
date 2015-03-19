7ce56e40c5f5d01f2c8fbec550e996a0b419ab98
Zhao Zhenlong <zhaozhl@inspur.com>
2015-03-19 06:18:22 -0400

    rerange config files

--- configs.py
+++ configs.py
@@ -12,21 +12,25 @@ EX_BR_INT = "eno2"
 
 CINDER_LVM_DISK = "/dev/sdb"
 
-LOCAL_OPENSTACK_DB = "./openstack-db"
+LOCAL_OPENSTACK_DB = "./scripts/openstack-db"
 OPENSTACK_DB = "/usr/local/bin/openstack-db"
+LOCAL_ADMIN_OPENRC= "./scripts/admin-openrc.sh"
+ADMIN_OPENRC = "/root/admin-openrc.sh"
+LOCAL_DEMO_OPENRC= "./scripts/demo-openrc.sh"
+DEMO_OPENRC = "/root/demo-openrc.sh"
 
 LOCAL_KEYSTONE_CONF = "./keystone.conf"
 KEYSTONE_CONF = "/etc/keystone/keystone.conf"
 
-LOCAL_GLANCE_API_CONF = "./glance-api.conf" 
+LOCAL_GLANCE_API_CONF = "./glance/glance-api.conf" 
 GLANCE_API_CONF = "/etc/glance/glance-api.conf"
-LOCAL_GLANCE_REGISTRY_CONF = "./glance-registry.conf"
+LOCAL_GLANCE_REGISTRY_CONF = "./glance/glance-registry.conf"
 GLANCE_REGISTRY_CONF = "/etc/glance/glance-registry.conf"
 
-LOCAL_NOVA_CONTROLLER_CONF = "./nova-controller.conf"
+LOCAL_NOVA_CONTROLLER_CONF = "./nova/nova-controller.conf"
 NOVA_CONTROLLER_CONF = "/etc/nova/nova.conf"
 
-LOCAL_NOVA_COMPUTE_CONF = "./nova-compute.conf"
+LOCAL_NOVA_COMPUTE_CONF = "./nova/nova-compute.conf"
 NOVA_COMPUTE_CONF = "/etc/nova/nova.conf"
 
 LOCAL_NEUTRON_CONTROLLER_CONF = "./neutron-controller/neutron.conf"
@@ -67,50 +71,13 @@ CINDER_BLOCK_CONF = "/etc/cinder/cinder.conf"
 LOCAL_HORIZON_CONF = "./horizon/local_settings"
 HORIZON_CONF = "/etc/openstack-dashboard/local_settings"
 
-LOCAL_ADMIN_OPENRC= "./admin-openrc.sh"
-ADMIN_OPENRC = "/root/admin-openrc.sh"
-
-LOCAL_DEMO_OPENRC= "./demo-openrc.sh"
-DEMO_OPENRC = "/root/demo-openrc.sh"
-
-
 AUTH_KEYS = "./authorized_keys"
 HOST_LIST = "/etc/hosts"
 
-LOCAL_CENTOS7_REPO = "./centos7.repo"
-LOCAL_EPEL7_REPO = "./epel7.repo"
+LOCAL_CENTOS7_REPO = "./repos/centos7.repo"
 CENTOS7_REPO = "/etc/yum.repos.d/CentOS-Base.repo"
+LOCAL_EPEL7_REPO = "./repos/epel7.repo"
 EPEL7_REPO = "/etc/yum.repos.d/epel.repo"
 
-LOCAL_CENTOS6_REPO = "./centos6.repo"
-LOCAL_EPEL6_REPO = "./epel6.repo"
-CENTOS6_REPO = "/etc/yum.repos.d/CentOS-Base.repo"
-EPEL6_REPO = "/etc/yum.repos.d/epel.repo"
-
-LOCAL_OPENSTACK_REPO = "./openstack.repo"
+LOCAL_OPENSTACK_REPO = "./repos/openstack.repo"
 OPENSTACK_REPO = "/etc/yum.repos.d/rdo-release.repo"
-
-LOCAL_IVIRTUAL_REPO = "./ivirtual.repo"
-IVIRTUAL_REPO = "/etc/yum.repos.d/ivirtual.repo"
-
-
-LOCAL_ADMIN_OPENRC= "./admin-openrc.sh"
-ADMIN_OPENRC = "/root/admin-openrc.sh"
-
-LOCAL_DEMO_OPENRC= "./demo-openrc.sh"
-DEMO_OPENRC = "/root/demo-openrc.sh"
-
-
-AUTH_KEYS = "./authorized_keys"
-HOST_LIST = "/etc/hosts"
-
-LOCAL_CENTOS7_REPO = "./centos7.repo"
-LOCAL_EPEL7_REPO = "./epel7.repo"
-CENTOS7_REPO = "/etc/yum.repos.d/CentOS-Base.repo"
-EPEL7_REPO = "/etc/yum.repos.d/epel.repo"
-
-LOCAL_OPENSTACK_REPO = "./openstack.repo"
-OPENSTACK_REPO = "/etc/yum.repos.d/rdo-release.repo"
-
-LOCAL_IVIRTUAL_REPO = "./ivirtual.repo"
-IVIRTUAL_REPO = "/etc/yum.repos.d/ivirtual.repo"
